2658ae180e6cf4f8de90159c149d4fbb972b1877
Matt Might <matt@might.net>
2022-12-06 06:56:31 -0600

    Day 6: C#

---
+++ day-6/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace signal;
+
+using System.IO;
+
+class Program
+{
+    // find the locaton in the string after the first span of the provided
+    // length in which all characters are distinct:
+    static int findDistinctSpan(string line, int span, int location = 0) {
+
+        // Check if the string is empty:
+        if (line == "")
+            return -1;
+
+        // Check if there is enough string left:
+        if (line.Length < span)
+            return -1;
+
+        // Grab the next span charaters:
+        string substring = line.Substring(0, span);
+
+        // Count the number of distinct characters in the span:
+        int distincts = substring.Distinct().Count();
+
+        // Check if every character was unique within the span:
+        if (distincts < span)
+
+            // Keep looking:
+            return findDistinctSpan(line.Substring(1), span, location + 1);
+
+        else
+            // Span found:
+            return location + span;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        string path = Directory.GetCurrentDirectory();
+
+        Console.WriteLine("part 1 test cases: ");
+        Console.WriteLine(findDistinctSpan("bvwbjplbgvbhsrlpgdmjqwftvncz", 4));
+        Console.WriteLine(findDistinctSpan("nppdvjthqldpwncqszvftbrmjlhg", 4));
+        Console.WriteLine(findDistinctSpan("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4));
+        Console.WriteLine(findDistinctSpan("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4));
+
+
+        Console.WriteLine("part 2 test cases: ");
+        Console.WriteLine(findDistinctSpan("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14));
+        Console.WriteLine(findDistinctSpan("bvwbjplbgvbhsrlpgdmjqwftvncz", 14));
+        Console.WriteLine(findDistinctSpan("nppdvjthqldpwncqszvftbrmjlhg", 14));
+        Console.WriteLine(findDistinctSpan("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14));
+        Console.WriteLine(findDistinctSpan("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14));
+
+
+        foreach (string line in System.IO.File.ReadLines("input.txt")) {
+            Console.WriteLine("part 1 answer: " + findDistinctSpan(line, 4));
+            Console.WriteLine("part 2 answer: " + findDistinctSpan(line, 14)) ;
+        }
+    }
+}
+
---
+++ day-6/README.md
@@ -0,0 +1,7 @@
+# Day 6
+
+I used C# for this day because I'd never programmed in it before.
+
+I used Visual Studio for Mac.
+
+I couldn't figure out how to invoke the C# compiler directly from the command line, so there is no Makefile.
